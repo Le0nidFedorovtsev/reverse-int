3e1863bc960dbc99e316eaa7a06fd026179b32e9
Leonid <103014729+Le0nidFedorovtsev@users.noreply.github.com>
2023-08-22 17:40:39 +0500

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-module.exports = function reverse (n) {return (parseFloat(n.toString().split('').reverse().join('')))}
+module.exports = function reverse (n) {
+    const text = n + ""
+    const t = text.split('')
+    if (t[0] === "-") {
+        t.shift()
+    } 
+    let r = []
+    let index;
+    for (index = 0; index < t.length; ++index) {
+        r.unshift(t[index])
+        console.log(t[index])
+    }
+    let yt = r.join('')
+    let result = +yt
+    return result}
